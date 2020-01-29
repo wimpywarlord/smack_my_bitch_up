b12d30a0c994cc4600768c43983884af3f9d294b
wimpywarlord <wimpywarlord@gmail.com>
2020-01-29 15:17:09 +0530

    Corrected project structure,added params

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-var mongoose = require("mongoose");
+
 
 var app = express();
 app.use(bodyParser.urlencoded({extended : true}));
-app.use(express.static("public"));
+app.use(express.static(__dirname + '/public'));
+
+app.get("/",function(req,res)
+	{
+		res.render("smack_my_bitch_up.ejs");
+	}
+);
 
 app.listen( process.env.PORT || 8000 , function(){
-	console.log("SERVER 3000 HAS STARTED");
+	console.log("SERVER 8000 HAS STARTED");
 });
